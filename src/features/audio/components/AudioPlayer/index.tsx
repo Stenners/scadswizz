44e8af2affd1401e2292d43031762cba9b883e3b
Thomas Eady <tomeady97@gmail.com>
2023-11-09 10:12:22 +1100

    fix build errors

--- src/features/audio/components/AudioPlayer/index.tsx
+++ src/features/audio/components/AudioPlayer/index.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useRef, useState } from "react";
 import { Button } from "../../../../common/Button";
-import AudioVisualizer from "../Visualizer";
+// import AudioVisualizer from "../Visualizer";
 
 interface AudioPlayerProps {
   src: string;
@@ -8,7 +8,7 @@ interface AudioPlayerProps {
 
 export const AudioPlayer: React.FC<AudioPlayerProps> = ({ src }) => {
   const audioRef = useRef<HTMLAudioElement>(null);
-  const [played, setPlayed] = useState(false);
+  //const [played, setPlayed] = useState(false);
   const [playing, setPlaying] = useState(false);
 
   useEffect(() => {
@@ -25,7 +25,7 @@ export const AudioPlayer: React.FC<AudioPlayerProps> = ({ src }) => {
       console.error("current is not available on audioRef");
       return;
     }
-    setPlayed(true);
+    //setPlayed(true);
     audioRef.current.play();
   };
 
