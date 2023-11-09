e114c446cde76fc40bc9843be7d701086797d89c
Thomas Eady <tomeady97@gmail.com>
2023-11-09 12:05:09 +1100

    add codename

--- src/common/Layout/index.tsx
+++ src/common/Layout/index.tsx
@@ -7,9 +7,11 @@ export const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <Transition>
       <div className="p-1 md:p-0 min-h-screen">
-        {/* <header className="h-14 w-full bg-emerald-400 flex space-between fixed">
-        <div className="h-full relative flex items-center">test</div>
-      </header> */}
+        <header className="h-14 w-full flex space-between fixed">
+          <div className="h-full relative text-xs opacity-50">
+            Project Papyrus v1.0
+          </div>
+        </header>
         <div className="h-14"></div>
         <main>
           <Outlet />
