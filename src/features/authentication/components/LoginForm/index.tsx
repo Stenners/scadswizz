7c8a50b28d8b208515d39371699f7a7199089785
Thomas Eady <tomeady97@gmail.com>
2023-11-09 13:25:53 +1100

    construction emojis

--- src/features/authentication/components/LoginForm/index.tsx
+++ src/features/authentication/components/LoginForm/index.tsx
@@ -49,11 +49,11 @@ export const LoginForm: React.FC = () => {
       />
       <FadeIn delay={0.15}>
         <div className="text-center w-full w-full">
-          <h1 className="text-xl">
+          <h1 className="text-xl mb-1">
             <strong className="text-emerald-500">SCADSWIZZ</strong>
             <br />
           </h1>
-          <p className="mb-10 opacity-50 text-sm">(demo)</p>
+          <p className="mb-10 opacity-75 text-sm">🚧 DEMO 🚧</p>
           <p className="mb-5">
             Let's get started! Try signing in with your <strong>SCA</strong>{" "}
             email...
