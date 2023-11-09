abc844cff6e3819a3b379a6a9a40d51695b44b41
Thomas Eady <tomeady97@gmail.com>
2023-11-09 14:55:30 +1100

    change codename

--- src/common/Layout/index.tsx
+++ src/common/Layout/index.tsx
@@ -10,8 +10,8 @@ export const Layout: React.FC<LayoutProps> = ({ children }) => {
     <Transition>
       <div className="p-2 md:p-0">
         <header className="h-14 w-full flex justify-between">
-          <div className="h-full relative text-xs opacity-50">
-            Project Papyrus alpha v0.0.0.1 (not even near finished)
+          <div className="h-full relative text-xs opacity-50 flex">
+            Project Papyrus alpha-alpha v0.0.00.0.0.0.1
           </div>
           {location.pathname !== "/login" && (
             <a className="h-full" href="/login">
