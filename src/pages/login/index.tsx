539929f8bb42fe423f964bc0ffbc5f5469365c98
Thomas Eady <tomeady97@gmail.com>
2023-11-09 13:07:46 +1100

    reduce bg opacity

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -5,7 +5,7 @@ export const LoginPage = () => {
     <>
       <div className="h-full w-full absolute top-0 left-0 z-10 overflow-x-hidden">
         <div
-          className="absolute w-[150%] h-[150%] opacity-[0.05] animate-pan-loop"
+          className="absolute w-[150%] h-[150%] opacity-[0.03] animate-pan-loop"
           style={{
             background: "url(/test.png)",
             backgroundRepeat: "repeat",
